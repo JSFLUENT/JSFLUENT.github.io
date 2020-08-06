56b56b30a85a821c73a6d8d7bbd3784e142c6ff0
djtinkers <djtinkers365@gmail.com>
2020-08-06 13:23:52 -0700

    question generators working next task more computation questions

--- ExpressionGenerator.js
+++ ExpressionGenerator.js
@@ -1,11 +1,11 @@
 class ExpressionGenerator {
     expression = "";
-    sameDataTypeSameOperators(n, datatype) {
+    sameDataTypeSameOperators(n, datatype, _op) {
         if (typeof datatype !== "string") { 
             datatype = new ValueGenerator().getDataType(datatype);
         }
         this.expression += new ValueGenerator().generate(datatype);
-        let op = Object.keys(ops.double)[Math.floor(Object.keys(ops.double).length * Math.random())];
+        let op = _op || Object.keys(ops.double)[Math.floor(Object.keys(ops.double).length * Math.random())];
         for (let i = 1; i < n; i++) {
             this.expression += ' ' + op + ' ' + new ValueGenerator().generate(datatype);
         }
@@ -23,11 +23,11 @@ class ExpressionGenerator {
         }
         return this.expression;
     }
-    diffDataTypeSameOperators(n) { 
+    diffDataTypeSameOperators(n,_op) { 
         this.expression += new ValueGenerator().generate();
-        let op = Object.keys(ops.double)[Math.floor(Object.keys(ops.double).length * Math.random())]
+        let op = _op || Object.keys(ops.double)[Math.floor(Object.keys(ops.double).length * Math.random())]
         for (let i = 1; i < n; i++) {
-            this.expression += op+ new ValueGenerator().generate()
+            this.expression += op + new ValueGenerator().generate()
         }
         return this.expression;
     }
@@ -36,7 +36,7 @@ class ExpressionGenerator {
         let op;
         for (let i = 1; i < n; i++) {
             op = Object.keys(ops.double)[Math.floor(Object.keys(ops.double).length * Math.random())]
-            this.expression += op+ new ValueGenerator().generate()
+            this.expression += op + new ValueGenerator().generate()
         }
         return this.expression;
     }
--- ValueGenerator.js
+++ ValueGenerator.js
@@ -3,13 +3,21 @@ class ValueGenerator {
     datatypes = {
         // datatypes // values
             boolean: ['true', 'false'],
-            number: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-            string: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            number: ['0','1','2','3','4','5','6','7','8','9'],
+            string: [
+                '"a"', '"b"', '"c"', '"d"', '"e"', '"f"', '"g"', '"h"',
+                '"i"', '"j"', '"k"', '"l"', '"m"', '"n"', '"o"', '"p"',
+                '"q"', '"r"', '"s"', '"t"', '"u"', '"v"', '"w"', '"x"',
+                '"y"', '"z"', '"A"', '"B"', '"C"', '"D"', '"E"', '"F"',
+                '"G"', '"H"', '"I"', '"J"', '"K"', '"L"', '"M"', '"N"',
+                '"O"', '"P"', '"Q"', '"R"', '"S"', '"T"', '"U"', '"V"',
+                '"W"', '"X"', '"Y"', '"Z"'
+              ],
             object: ['{}', "{ a: 'apple' }", '{ b: [] }', '{ c: 12 }', '{ d: {} }', '{ e: 3 }', '{ 0: true }', "{9:''}"],
             array: ['[]', '[1]', '[32]', "['a', 'z']", "['13', 12]", "['a', 'b', 'c']", "[{}]", "[false, true]"],
-            function: ['function () { }', 'function () { console.log(datatypes) }'],
+            function: [],
             undefined: ['undefined', 'null'],
-            symbol: []
+            // symbol: []
     };
     getDataType(datatype) { 
         return datatype || Object.keys(this.datatypes)[Math.floor(Math.random() * Object.keys(this.datatypes).length)];
