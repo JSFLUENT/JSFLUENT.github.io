9fa3ba40036f624fc04f9679a6d24bd8c9370422
djtinkers <djtinkers365@gmail.com>
2020-08-13 12:37:30 -0700

    section-arraybuiltinmethods organized

---
+++ section-arraybuiltinmethods.js
@@ -0,0 +1,37 @@
+let attrs = `
+length
+pop
+push
+shift
+unshift
+slice
+splice
+fill
+copyWithin
+reverse
+sort
+forEach
+every
+some
+filter
+map
+includes
+find
+indexOf
+findIndex
+lastIndexOf
+concat
+join
+toLocaleString
+toString
+reduce
+reduceRight
+flat
+flatMap
+entries
+values
+keys`.split("\n");
+
+// Array.from
+// Array.isArray
+// Array.prototype
